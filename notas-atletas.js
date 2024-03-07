232aedf04abd5e5a07df31d1655d2ba4b6a26977
Leonardo <leonardobadell@hotmail.com>
2024-03-06 23:37:28 -0300

    Modificando funções e métodos no projeto

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,52 +1,41 @@
+// Notas dos atletas
 let atletas = [
-    {
-      nome: "Cesar Abascal",
-      notas: [10, 9.34, 8.42, 10, 7.88]
-    },
-    {
-      nome: "Fernando Puntel",
-      notas:  [8, 10, 10, 7, 9.33]
-    },
-    {
-      nome: "Daiane Jelinsky",
-      notas: [7, 10, 9.5, 9.5, 8]
-    },
-    {
-      nome: "Bruno Castro",
-      notas: [10, 10, 10, 9, 9.5]
-    }
-   ];
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+];
 
-let nomeAtletas = 0;
-let obterMedia = 0;
-let notasResultado =0;
+// Método forEach aplicado com os métodos slice() e sort() como recomendado
+atletas.forEach(function(atleta) {
+  let notas = atleta.notas.slice().sort(function(a, b) { 
+    return a - b; 
+  });
 
-for (let i = 0; i < atletas.length; i++) {
-nomeAtletas = atletas.map(function(nomes) {
-    return nomes.nome
-})
+  let notasIntermediarias = notas.slice(1, notas.length - 1);
 
-notasResultado = atletas.map(function(notas) {
-    let total = notas.notas.slice();
-    total.sort(function(a, b) {
-        return a - b
-    })
-    return total;
-})
-}
+  // Método reduce
+  let soma = notasIntermediarias.reduce(function(total, atual) { 
+    return total + atual; 
+  }, 0);
 
-for(let i= 0; i < atletas.length; i++) {
-    switch (i) {
-        case i:
-            console.log("Atleta: " + nomeAtletas[i])
-            console.log("Resultado: " + notasResultado[i])
-            let notasAtletas = notasResultado[i].slice(1, notasResultado[i].length - 1);
-            let soma = notasResultado.reduce(function(total, atual) {
-                return total + atual
-            })
-            let media = soma / notasAtletas.length;
-            console.log(obterMedia&{media})
-            console.log(console.log("Atleta não cadastrado")
-            )
-    }
-}
+  // Cálculo da media
+  let media = soma / notasIntermediarias.length;
+
+  // Imprimindo os resultados das variáveis com console.log()
+  console.log(`Atleta:  ${atleta.nome}`);
+  console.log(`Notas Obtidas:  ${notasIntermediarias.join(',')}`);
+  console.log(`Média Válida:   ${media}\n`);
+});
